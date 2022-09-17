60e2497b7c7bcf71db3fa1166846eaa46e4cb1dd
buz321 <107760647+buz321@users.noreply.github.com>
2022-09-18 08:43:18 +0900

    Update Stored Routines.sql

--- Stored Routines.sql
+++ Stored Routines.sql
@@ -239,3 +239,14 @@ SELECT EMP_INFO('Aruna', 'Journel');
 FUNCTIONS can return a single value only (not appropriate) */
 
 /* DECLARE is a keyword that can be used when creating local variables only */
+
+
+/* Session variable = a variable that exists only for the session in which you are operating */
+
+/* Gloval variable = system variables are required: 
+.max_connections() - indicates the maximum number of connections to a server that can be established at a certain point in time,
+.max_join_size() - sets the maximum memory space allocated for the joins created by a certain connection */
+
+/* To use GLOBAL variables (GLOBAL or @@global)*/
+SET GLOBAL max_connections = 10000;
+SET @@global max_connections = 1;
